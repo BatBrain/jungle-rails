de111940cd7ecbe317936bab43e890acaf3d6fcb
Sol <sol@Sols-MacBook-Pro.local>
2016-10-19 17:10:40 -0400

    tacky work-around replace with secure admin auth needed

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < ApplicationController
+  @admin_name = ENV['ADMIN_NAME']
+  @admin_password = ENV['ADMIN_PASSWORD']
+  http_basic_authenticate_with name: 'Jungle', password: 'book'
   before_filter :authorize
   def show
   end
